292ee9f5f88d14e9f6a5adb11946078dd1a33a7c
Tu Kim Hoang <tukimhoang@MacBook-Pro.local>
2024-12-30 14:04:00 +0700

    update chuc-mung-nam-moi

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    // Thêm lớp 'visible' sau khi trang được tải để kích hoạt hiệu ứng
+    document.getElementById('myImage').classList.add('visible');
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         center: true,
       },
       backgroundImage: {
-        'custom-pattern': "url('../img/bg-noel1.jpg')",
+        'custom-pattern': "url('../img/bg-tet.jpg')",
         'custom-pattern2': "url('../img/bg-noel3.jpg')",
       },
       // 
@@ -20,11 +20,34 @@ module.exports = {
           '100%': { opacity: 1 },
         },
       },
-
+      // 
+      keyframes: {
+        typing: {
+          'from': { width: '0' },
+          'to': { width: '100%' },
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' },
+        },
+      },
+      animation: {
+        typing: 'typing 4s steps(20, end)',
+        blink: 'blink 0.5s step-end infinite',
+      },
       
-      
-      
-      
+      // phúc, lộc, thọ, khang
+      keyframes: {
+        slideDown: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'slide-down': 'slideDown 8.5s ease-out',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'], // Font chữ đẹp
+      },
     },
   },
   plugins: [require('flowbite/plugin')],
